a0057e7e31b8205a6db01c29dccfe3ced9d4540a
ManziMunyambo <munyamb@uwindsor.ca>
2026-03-29 19:47:00 -0400

    Main.java now calls Manzis' UI

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,24 @@
  * -> This is the main class to run the finance tracker application.
  */
 
-import javax.swing.*;
+import com.formdev.flatlaf.FlatLightLaf;
 
 
 public class Main {
         public static void main(String[] args) {
-        new MainInterface();  // Constructor handles everything now
+        //new MainInterface();  // Constructor handles everything now
+
+        //try launching the cooler UI
+        try{
+                FlatLightLaf.setup();
+        } catch (Exception e){
+                System.out.println("Error! " + e.getMessage());
+                e.printStackTrace();
+        }
+
+        javax.swing.SwingUtilities.invokeLater(() -> {
+                UserInterface gui = new UserInterface();
+                gui.setVisible(true);
+        });
     }
 }
